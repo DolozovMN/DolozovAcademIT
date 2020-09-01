569ed00a3365b9b8962b2e40739783f877ca3d9f
admin <dmnleba@gmail.com>
2020-09-01 16:46:49 +0600

    Звдача Vector (Начало)

---
+++ Vector/src/ru/academit/dolozov/vector_main/Main.java
@@ -0,0 +1,18 @@
+package ru.academit.dolozov.vector_main;
+
+import ru.academit.dolozov.vector.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector vector1 = new Vector(5);
+        double[] array1 = {1, 2, 3, 4};
+        double[] array2 = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+
+        Vector vector2 = new Vector (array1);
+        Vector vector3 = new Vector(10, array2);
+
+        System.out.println(vector1.toString() +" размерность вектора: " +vector1.getSize());
+        System.out.println(vector2.toString() +" размерность вектора: " +vector2.getSize());
+        System.out.println(vector3.toString() +" размерность вектора: " +vector3.getSize());
+    }
+}
