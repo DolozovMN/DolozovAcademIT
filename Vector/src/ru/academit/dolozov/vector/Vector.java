5261ea7fdd9f3d226c2daf111930904390fc5e8a
DMN <dmnleba@gmail.com>
2020-09-01 21:36:39 +0700

    Задача Вектор (часть 1)

--- Vector/src/ru/academit/dolozov/vector/Vector.java
+++ Vector/src/ru/academit/dolozov/vector/Vector.java
@@ -46,6 +46,30 @@ public class Vector {
     }
 
     @Override
+    public int hashCode(){
+        final int prime = 29;
+        int hash = 1;
+        for (double element : arrayVector) {
+            hash = prime * hash + Double.hashCode(element);
+        }
+        return hash;
+    }
+
+    @Override
+    public boolean equals (Object o){
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null || o.getClass() != this.getClass() || this.hashCode() != o.hashCode()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+        return (Arrays.equals(this.arrayVector, vector.arrayVector));
+    }
+
+    @Override
     public String toString() {
         return Arrays.toString(arrayVector);
     }
--- Vector/src/ru/academit/dolozov/vector_main/Main.java
+++ Vector/src/ru/academit/dolozov/vector_main/Main.java
@@ -11,10 +11,14 @@ public class Main {
         Vector vector2 = new Vector(array1);
         Vector vector3 = new Vector(10, array2);
         Vector vector4 = new Vector(vector2);
+        Vector vector5 = new Vector(array1);
 
-        System.out.println(vector1.toString() + " размерность вектора: " + vector1.getSize());
-        System.out.println(vector2.toString() + " размерность вектора: " + vector2.getSize());
-        System.out.println(vector3.toString() + " размерность вектора: " + vector3.getSize());
-        System.out.println(vector4.toString() + " размерность вектора: " + vector4.getSize());
+        System.out.println(vector1.toString() + " размерность вектора: " + vector1.getSize() + " хэш-код: " + vector1.hashCode());
+        System.out.println(vector2.toString() + " размерность вектора: " + vector2.getSize() + " хэш-код: " + vector2.hashCode());
+        System.out.println(vector3.toString() + " размерность вектора: " + vector3.getSize() + " хэш-код: " + vector3.hashCode());
+        System.out.println(vector4.toString() + " размерность вектора: " + vector4.getSize() + " хэш-код: " + vector4.hashCode());
+        System.out.println(vector5.toString() + " размерность вектора: " + vector5.getSize() + " хэш-код: " + vector5.hashCode());
+
+        System.out.println(vector2.equals(vector5));
     }
 }
