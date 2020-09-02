eadcd3a5b829dcc18c2c75c3e0c1ef24dcbc4b2c
admin <dmnleba@gmail.com>
2020-09-02 14:42:09 +0600

    Звдача Vector (Для проверки)

--- Vector/src/ru/academit/dolozov/vector/Vector.java
+++ Vector/src/ru/academit/dolozov/vector/Vector.java
@@ -41,12 +41,169 @@ public class Vector {
         }
     }
 
+    public void addVector(Vector vector) {
+        int n1 = arrayVector.length;
+        int n2 = vector.arrayVector.length;
+
+        if (n1 > n2) {
+            for (int i = 0; i < n1; i++) {
+                if (i < n2) {
+                    arrayVector[i] = arrayVector[i] + vector.arrayVector[i];
+                } else {
+                    arrayVector[i] = arrayVector[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < n1; i++) {
+                arrayVector[i] = arrayVector[i] + vector.arrayVector[i];
+            }
+        }
+    }
+
+    public void minusVector(Vector vector) {
+        int n1 = arrayVector.length;
+        int n2 = vector.arrayVector.length;
+
+        if (n1 > n2) {
+            for (int i = 0; i < n1; i++) {
+                if (i < n2) {
+                    arrayVector[i] = arrayVector[i] - vector.arrayVector[i];
+                } else {
+                    arrayVector[i] = arrayVector[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < n1; i++) {
+                arrayVector[i] = arrayVector[i] - vector.arrayVector[i];
+            }
+        }
+    }
+
+    public void multiplyOnScalar(int scalar) {
+        int n = arrayVector.length;
+
+        for (int i = 0; i < n; i++) {
+            arrayVector[i] = arrayVector[i] * scalar;
+        }
+    }
+
+    public void expandVector() {
+        int n = arrayVector.length;
+
+        for (int i = 0; i < n; i++) {
+            arrayVector[i] = -1 * arrayVector[i];
+        }
+    }
+
+    public double getVectorElement(int index) {
+        int n = arrayVector.length;
+
+        if (index < 0 || index >= n) {
+            throw new IllegalArgumentException("Недопустимый индекс элемента вектора");
+        }
+
+        return arrayVector[index];
+    }
+
+    public double getVectorLength() {
+        int n = arrayVector.length;
+        double sum = 0;
+
+        for (double arrayElement : arrayVector) {
+            sum = sum + arrayElement;
+        }
+
+        return sum;
+    }
+
+    public Vector getVectorSum(Vector vector) {
+        int n1 = arrayVector.length;
+        int n2 = vector.arrayVector.length;
+        int nMax = Math.max(n1, n2);
+        double[] arraySum = new double[nMax];
+
+        if (n1 > n2) {
+            for (int i = 0; i < nMax; i++) {
+                if (i < n2) {
+                    arraySum[i] = arrayVector[i] + vector.arrayVector[i];
+                } else {
+                    arraySum[i] = arrayVector[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < nMax; i++) {
+                if (i < n1) {
+                    arraySum[i] = arrayVector[i] + vector.arrayVector[i];
+                } else {
+                    arraySum[i] = vector.arrayVector[i];
+                }
+            }
+        }
+
+        return new Vector(arraySum);
+    }
+
+    public Vector getVectorDifference(Vector vector) {
+        int n1 = arrayVector.length;
+        int n2 = vector.arrayVector.length;
+        int nMax = Math.max(n1, n2);
+        double[] arrayDiff = new double[nMax];
+
+        if (n1 > n2) {
+            for (int i = 0; i < nMax; i++) {
+                if (i < n2) {
+                    arrayDiff[i] = arrayVector[i] - vector.arrayVector[i];
+                } else {
+                    arrayDiff[i] = arrayVector[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < nMax; i++) {
+                if (i < n1) {
+                    arrayDiff[i] = arrayVector[i] - vector.arrayVector[i];
+                } else {
+                    arrayDiff[i] = 0 - vector.arrayVector[i];
+                }
+            }
+        }
+
+        return new Vector(arrayDiff);
+    }
+
+    public Vector getVectorComposition(Vector vector) {
+        int n1 = arrayVector.length;
+        int n2 = vector.arrayVector.length;
+        int nMax = Math.max(n1, n2);
+        double[] arrayComposition = new double[nMax];
+
+        for (int i = 0; i < nMax; i++) {
+            double factor1;
+            double factor2;
+
+            if (i < n1) {
+                factor1 = arrayVector[i];
+            } else {
+                factor1 = 0;
+            }
+
+            if (i < n2) {
+                factor2 = vector.arrayVector[i];
+            } else {
+                factor2 = 0;
+            }
+
+            arrayComposition[i] = factor1 * factor2;
+        }
+
+        return new Vector(arrayComposition);
+    }
+
     public int getSize() {
         return arrayVector.length;
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         final int prime = 29;
         int hash = 1;
         for (double element : arrayVector) {
@@ -56,7 +213,7 @@ public class Vector {
     }
 
     @Override
-    public boolean equals (Object o){
+    public boolean equals(Object o) {
         if (o == this) {
             return true;
         }
--- Vector/src/ru/academit/dolozov/vector_main/Main.java
+++ Vector/src/ru/academit/dolozov/vector_main/Main.java
@@ -7,18 +7,47 @@ public class Main {
         Vector vector1 = new Vector(5);
         double[] array1 = {1, 2, 3, 4};
         double[] array2 = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+        double[] array3 = {0, 2.5, 3.5, 4.5, 10};
 
         Vector vector2 = new Vector(array1);
-        Vector vector3 = new Vector(10, array2);
+        Vector vector3 = new Vector(6, array2);
         Vector vector4 = new Vector(vector2);
         Vector vector5 = new Vector(array1);
+        Vector vector6 = new Vector(array3);
 
-        System.out.println(vector1.toString() + " размерность вектора: " + vector1.getSize() + " хэш-код: " + vector1.hashCode());
-        System.out.println(vector2.toString() + " размерность вектора: " + vector2.getSize() + " хэш-код: " + vector2.hashCode());
-        System.out.println(vector3.toString() + " размерность вектора: " + vector3.getSize() + " хэш-код: " + vector3.hashCode());
-        System.out.println(vector4.toString() + " размерность вектора: " + vector4.getSize() + " хэш-код: " + vector4.hashCode());
-        System.out.println(vector5.toString() + " размерность вектора: " + vector5.getSize() + " хэш-код: " + vector5.hashCode());
+        System.out.println("Вектор 1: " + vector1.toString() + " размерность вектора: " + vector1.getSize());
+        System.out.println("Вектор 2: " + vector2.toString() + " размерность вектора: " + vector2.getSize());
+        System.out.println("Вектор 3: " + vector3.toString() + " размерность вектора: " + vector3.getSize());
+        System.out.println("Вектор 4: " + vector4.toString() + " размерность вектора: " + vector4.getSize());
+        System.out.println("Вектор 5: " + vector5.toString() + " размерность вектора: " + vector5.getSize());
+        System.out.println("Вектор 6: " + vector6.toString() + " размерность вектора: " + vector6.getSize());
 
-        System.out.println(vector2.equals(vector5));
+        System.out.println("Проверка равенства: " + vector2.equals(vector5));
+        System.out.println();
+
+        System.out.println("Длина вектора 6: " + vector6.getVectorLength());
+
+        Vector vectorSum = vector2.getVectorSum(vector6);
+        System.out.println("Суммарный вектор 2 и 6 (Новый): " + vectorSum.toString());
+
+        Vector vectorDiff = vector2.getVectorDifference(vector6);
+        System.out.println("Вектор разности (Новый): " + vectorDiff.toString());
+
+        Vector vectorComposition = vector2.getVectorComposition(vector6);
+        System.out.println("Вектор произведения (Новый): " + vectorComposition.toString());
+
+        vector4.expandVector();
+        System.out.println("Разворот вектора 4: " + vector4.toString());
+
+        vector2.minusVector(vector6);
+        System.out.println("Вектор 2 - 6: " + vector2.toString());
+
+        vector6.addVector(vector5);
+        System.out.println("Вектор 6 + 5: " + vector6.toString());
+
+        vector5.multiplyOnScalar(3);
+        System.out.println("Умножение вектора 5 на скаляр:" + vector5.toString());
+
+        System.out.println("Элемент вектора: " + vector6.getVectorElement(3));
     }
 }
